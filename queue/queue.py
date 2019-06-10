f11b291e43f632fd5eb6e9f1d5c707c26b1dac88
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-06-10 17:16:34 -0400

    First pass at linked list solution

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,60 @@
+# Queue as an array
+# class Queue:
+#   def __init__(self):
+#     self.size = 0
+#     # what data structure should we
+#     # use to store queue elements?
+#     self.storage = []
+
+#   def enqueue(self, item):
+#     self.storage.insert(0, item)
+#     self.size += 1
+  
+#   def dequeue(self):
+#     if(self.size > 0):
+#       self.storage.pop()
+#       self.size -= 1
+
+#   def len(self):
+#     return len(self.storage)
+#     return self.size
+
+
+
+# Queue as a Linked List
+class Node:
+  def __init__(self, value=None, nextNode=None, prevNode=None):
+    self.value = value
+    self.nextNode = nextNode
+    self.prevNode = prevNode
+
 class Queue:
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.head = None
+    self.tail = None
 
   def enqueue(self, item):
-    pass
-  
+    enqueued = Node(item, self.head)
+    self.head.prevNode = enqueued
+    self.head = enqueued
+    self.size += 1
+
   def dequeue(self):
-    pass
+    self.tail = self.tail.prevNode
+    self.tail.nextNode = None
+    self.size -= 1
 
   def len(self):
-    pass
+    # return self.size
+    if(self.head == None):
+      return 0
+    else:
+      return _countSize(self.head)
+
+  def _countSize(self, node):
+    if(node.nextNode == None):
+      return 1
+    return 1  + _countSize(node.nextNode)
