f4e6c74bc134b48c8fd6c67171d218add373fe40
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-06-11 15:58:57 -0400

    Initial solution for bst

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,52 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    # value is greater or less than, move right or left.  If the same, do nothing
+    # When moving left or right, check for None.  If None is found, create the node
+    # Else, move to next node to repeat
+    if(value > self.value):
+      if(self.right == None):
+        # Create node
+      else:
+        self.right.insert(value)
+    elif(value < self.value):
+      if(self.right == None):
+        # Create node
+      else:
+        self.left.insert(value)
+    else:
+      return
+    
 
   def contains(self, target):
-    pass
+    # Value is greater or less than, move right or left.  If the same, return True
+    # When moving, check for None.  If found, return False.  Else, recursion
+    if(target > self.value):
+      if(self.right == None):
+        return False
+      else:
+        self.right(target)
+    elif(target < self.value):
+      if(self.left == None):
+        return False
+      else:
+        self.left(target)
+    else:
+      return True
+
 
   def get_max(self):
-    pass
+    # Greater numbers to the right, always check right until None.
+    right = self.right
+    while right != None:
+      if(right.right == None):
+        return self.value
+      else:
+        right = right.right
 
   def for_each(self, cb):
-    pass
+    if(self.left != None):
+      self.left.for_each(cb)
+    if(self.right != None):
+      self.right.for_each(cb)
+    return cb(self.value)
